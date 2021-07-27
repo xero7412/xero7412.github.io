a3c5ce31aca775eb4574e3b3ec3453baad4780d2
Toshi Sharma <Toshi@BPSSMBA037s-MacBook-Air.local>
2021-07-28 01:21:51 +0530

    Updates

---
+++ precache-manifest.2d0b73233b8e01e31769b8c5ae873d79.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1d7dc14f8f2d90e7306d63be9d924f",
+    "url": "/xero7412.github.io/index.html"
+  },
+  {
+    "revision": "e935293d3d1fb952ff52",
+    "url": "/xero7412.github.io/static/css/main.3360fc62.chunk.css"
+  },
+  {
+    "revision": "045a4b58b9b6e85a49cf",
+    "url": "/xero7412.github.io/static/js/2.24dec150.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/xero7412.github.io/static/js/2.24dec150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e935293d3d1fb952ff52",
+    "url": "/xero7412.github.io/static/js/main.57c779cc.chunk.js"
+  },
+  {
+    "revision": "a15403cc937dfbf9c2b3",
+    "url": "/xero7412.github.io/static/js/runtime-main.bb9f6bde.js"
+  },
+  {
+    "revision": "a542b86bb468d1c3b4703f94a83ac543",
+    "url": "/xero7412.github.io/static/media/DThree.a542b86b.svg"
+  },
+  {
+    "revision": "c180896e00da0296bb575b84b842cb5a",
+    "url": "/xero7412.github.io/static/media/MongoDB.c180896e.svg"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/xero7412.github.io/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/xero7412.github.io/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/xero7412.github.io/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/xero7412.github.io/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "c0fafa8397437c95848724aed686d63b",
+    "url": "/xero7412.github.io/static/media/Poppins-Thin.c0fafa83.ttf"
+  },
+  {
+    "revision": "6e03d1db72e9acf257bd5c349684f5cb",
+    "url": "/xero7412.github.io/static/media/SQL.6e03d1db.svg"
+  },
+  {
+    "revision": "ca6dd3f1ed6b9681e6eddef6c71fbd22",
+    "url": "/xero7412.github.io/static/media/VertX.ca6dd3f1.svg"
+  },
+  {
+    "revision": "af162edd43a03a9b2cad16033800e259",
+    "url": "/xero7412.github.io/static/media/icomoon.af162edd.ttf"
+  },
+  {
+    "revision": "a9512904099fc1f7ce229ec6df219a31",
+    "url": "/xero7412.github.io/static/media/toshiSharma.a9512904.jpg"
+  }
+]);
